ade643997bd201a13d6d5280d1fc43cb9d872fe6
SenLu03 <senlu03@gmail.com>
2025-05-06 17:33:03 +0700

    feat: update settings

---
+++ src/logic_app/common/settings/models/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from .card_align import CardAlignSettings
+from .chromadb import ChromaDB
+from .pg import PostgresSettings
+__all__=['PostgresSettings', 'ChromaDB', 'CardAlignSettings']
