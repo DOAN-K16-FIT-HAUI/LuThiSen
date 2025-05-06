df2c6679e1852e6f240b943bdd389157cbbd6da5
SenLu03 <senlu03@gmail.com>
2025-05-06 17:31:44 +0700

    feat: update settings

--- src/logic_app/common/settings/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from .card_align import CardAlignSettings
-from .chromadb import ChromaDB
-from .pg import PostgresSettings
-__all__=['PostgresSettings', 'ChromaDB', 'CardAlignSettings']
--- src/logic_app/common/settings/models/chromadb.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from common.bases import BaseModel
-
-
-class ChromaDB(BaseModel):
-    name: str
-    path: str
