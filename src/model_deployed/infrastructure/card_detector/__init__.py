2b9a058dbdb7cb7c294ff19e4cc38a33d2a1561c
SenLu03 <senlu03@gmail.com>
2025-05-14 09:24:54 +0700

    feat: add card detect

---
+++ src/model_deployed/infrastructure/card_detector/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from .card_detector import CardDetectorModel
+from .card_detector import CardDetectorModelInput
+from .card_detector import CardDetectorModelOutput
+
+__all__ = ['CardDetectorModel', 'CardDetectorModelInput', 'CardDetectorModelOutput']
